8276a04c4b214e5a588d80f44e875e4ccce5c97c
Steve Kedzie <seal.team.smyhk@gmail.com>
2019-09-15 13:27:08 -0700

    Update shop data and group like items

--- src/components/custom-button/custom-button.component.js
+++ src/components/custom-button/custom-button.component.js
@@ -2,9 +2,16 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
-const CustomBotton = ({ children, isGoogleSignIn, ...otherProps }) => (
+const CustomBotton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
   <button
-    className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+    className={`${inverted ? 'inverted' : ''}  ${
+      isGoogleSignIn ? 'google-sign-in' : ''
+    } custom-button`}
     {...otherProps}
   >
     {children}
---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,17 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  // Look for duplicate item
+  const existingCartItem = cartItems.find(
+    cartItem => cartItem.id === cartItemToAdd.id
+  );
+
+  // Return new array with updated quantity of duplicate item found
+  if (existingCartItem) {
+    return cartItems.map(cartItem =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  // Add item to cart with initial quantity
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+};
