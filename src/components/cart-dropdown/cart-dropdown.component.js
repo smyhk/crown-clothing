839ab5714bad60fb218b8c8aa1edc86310718103
Steve Kedzie <seal.team.smyhk@gmail.com>
2019-09-16 10:24:23 -0700

    Use resect for cart memoization

--- src/components/cart-dropdown/cart-dropdown.component.js
+++ src/components/cart-dropdown/cart-dropdown.component.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import CustomButton from '../custom-button/custom-button.component';
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import CartItem from '../cart-item/cart-item.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './cart-dropdown.styles.scss';
 
@@ -17,8 +19,8 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  cartItems
+const mapStateToProps = state => ({
+  cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,22 @@
+// Caches cart item quantity for memoization to prevent unnecessary re-rendering
+import { createSelector } from 'reselect';
+
+// Get a reference to the cart in the global state object
+const selectCart = state => state.cart;
+
+// Get cart items
+export const selectCartItems = createSelector(
+  [selectCart], // Cart object
+  cart => cart.cartItems
+);
+
+// Use selectCartItems to reduce the quantities of cart items
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
