41054099602e8275d23b1c3a5c0275ce354a0d9e
Steve Kedzie <seal.team.smyhk@gmail.com>
2019-09-25 10:46:20 -0700

    Move collection data to redux store

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,7 @@ import storage from 'redux-persist/lib/storage'; // local storage
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import shopReducer from './shop/shop.reducer';
 import directoryReducer from './directory/directory.reducer';
 
 const persistConfig = {
@@ -15,6 +16,7 @@ const persistConfig = {
 const rootReducer = combineReducers({
   user: userReducer,
   cart: cartReducer,
+  shop: shopReducer,
   directory: directoryReducer
 });
 
