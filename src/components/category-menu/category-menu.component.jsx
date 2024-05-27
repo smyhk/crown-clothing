9d8ff433d6c4fcc3c465a037c34c0c717577c458
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-05-27 13:36:18 -0700

    Implement initial product menu page

---
+++ src/components/category-menu/category-menu.component.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+import CategoryItem from '../category-item/category-item.component';
+
+import './category-menu.styles.scss';
+
+const CategoryMenu = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map(category => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default CategoryMenu;
