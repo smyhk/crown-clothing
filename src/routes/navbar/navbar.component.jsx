c46f21817e663facf61c0c6d7db79bf34997e5cf
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-05-28 13:05:08 -0700

    Implement basic routing for Navbar and Home

---
+++ src/routes/navbar/navbar.component.jsx
@@ -0,0 +1,25 @@
+import { Outlet, Link } from 'react-router-dom';
+
+import CrownLogo from '../../assets/crown.svg';
+
+import './navbar.styles.scss';
+
+const NavBar = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <img className="logo" src={CrownLogo} alt="crown link" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default NavBar;
