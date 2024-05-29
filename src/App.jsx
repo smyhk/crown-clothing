4d52b70b6f0abbf4210861777134646ed49c2f83
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-05-29 12:09:53 -0700

    Use firebase and test basic sign-in route

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import NavBar from './routes/navbar/navbar.component';
+import SignIn from './routes/sign-n/sign-in.component';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<NavBar />}>
         <Route index element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
