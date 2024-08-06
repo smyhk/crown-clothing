ee86387bbf2cfc9aea375226b11c20d3d5daf535
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-08-05 18:19:08 -0700

    Start shop page and data retrieval

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
+import Shop from './routes/shop/shop.component';
 import NavBar from './routes/navbar/navbar.component';
 import Authentication from './routes/authentication/authentication.component';
 
@@ -9,6 +10,7 @@ const App = () => {
     <Routes>
       <Route path="/" element={<NavBar />}>
         <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
