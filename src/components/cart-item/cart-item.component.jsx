878296db7ef2304cde9bc07606ea6ad86a04cc8b
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-08-06 19:52:01 -0700

    Add products items to cart

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,14 @@
+import './cart-item.styles.scss';
+
+const CartItem = ({ cartIem }) => {
+  const { name, quantity } = cartIem;
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <span>{quantity}</span>
+    </div>
+  );
+};
+
+export default CartItem;
