4edf1c4578bb0d8fe58c719c27594e03cd30fd86
Steve Kedzie <seal.team.smyhk@gmail.com>
2024-08-23 12:35:34 -0700

    Fix categories not rendering

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useState, useEffect } from 'react';
+
+// import { addCollectionAndDocuments } from '../utils/firebase/firebase.utils.js';
+import { getCategoriesAndDocuments } from '../utils/firebase/firebase.utils';
+
+// import SHOP_DATA from '../shop-data.js';
+
+export const CategoriesContext = createContext({
+  categoriesMap: {},
+});
+
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
+
+  // Add categoried and associated data to firesore - only run once!
+  // useEffect(() => {
+  //   addCollectionAndDocuments('categories', SHOP_DATA);
+  // });
+
+  useEffect(() => {
+    async function getCategories() {
+      const categoryMap = await getCategoriesAndDocuments();
+      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
+    }
+
+    getCategories();
+  }, []);
+
+  const value = { categoriesMap };
+
+  return (
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
+  );
+};
